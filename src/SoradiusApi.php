15040f1c45d127610356bde01a8f00fe5733d727
geowan <geoffreymwangi5378@gmail.com>
2021-04-21 16:00:50 +0300

    new fix

--- src/SoradiusApi.php
+++ src/SoradiusApi.php
@@ -1,22 +1,45 @@
 <?php
+
 namespace SoradiusApi;
 
 use GuzzleHttp\Client;
 
 class SoradiusApi
 {
-	const BASE_DOMAIN = "soradius.com";
+    const BASE_DOMAIN = "soradius.com";
 
-	protected $apiKey;
+    protected $apiKey;
+    protected $baseDomain;
+    public $sandbox;
 
-	public $baseUrl;
+    public $baseUrl;
 
-	public function __construct($apiKey)
-	{
+    public function __construct($apiKey, $sandbox = false)
+    {
         $this->baseDomain = self::BASE_DOMAIN;
-
-		$this->baseUrl = "https://api." . $this->baseDomain . "/api/v1/";
-
-		//verify the api key is valid and then receive smtp credentials
-	}
+        $this->apiKey = $apiKey;
+        $this->sandbox = $sandbox;
+        $this->baseUrl = "https://api." . $this->baseDomain . "/api/v1/";
+    }
+    /**
+     * Sends email to soradius api
+     * @param $from
+     * @param $to
+     * @param $htmlBody
+     * @param $subject
+     */
+    public function sendEmail($from, $to, $subject, $htmlBody)
+    {
+        $client = new Client();
+        return $client->request('POST', $this->baseUrl . "/email/default/send-email", [
+            'body' => [
+                "from" => $from,
+                "to" => $to,
+                "subject" => $subject,
+                "sandbox" => $this->sandbox,
+                "html_body" => $htmlBody,
+                "apiKey" => $this->apiKey,
+            ]
+        ]);
+    }
 }
